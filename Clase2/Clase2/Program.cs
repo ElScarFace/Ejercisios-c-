352514cd452cb26eb124b78144898773c45ccfd7
Renzo <64338288+Ilomilo18@users.noreply.github.com>
2022-06-13 18:07:10 -0500

    Add files via upload

---
+++ Clase2/Clase2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1, n2, n3, nmy, nmn;
+            string nombre, condicion;
+
+            Console.WriteLine("Escriba su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escriba la 1º nota: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba la 2º nota: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba la 3º nota: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            double prom;
+            prom = (n1 + n2 + n3) / 3;
+
+            if (prom <= 5)
+            {
+                condicion = "Pesimo";
+            }
+            else if (prom <= 10)
+            {
+                condicion = "Malo";
+            }
+            else if (prom <= 15)
+            {
+                condicion = "Regular";
+            }
+            else if (prom <= 19)
+            {
+                condicion = "Bueno...";
+            }
+            else
+            {
+                condicion = "Excelente!!!";
+            }
+
+            nmy = n1;
+            if (n2 >= nmy)
+            {
+                nmy = n2;
+            }
+            else if (n3 >= nmy)
+            {
+                nmy = n3;
+            }
+            else
+            {
+                nmy = n1;
+            }
+
+           
+            nmn = n1;
+            if (n2 < nmn)
+            {
+                nmn = n2;
+            }
+            else if (n3 < nmn)
+            {
+                nmn = n3;
+            }
+            else
+            {
+                nmn = n1;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Nombre:     " + nombre);
+            Console.WriteLine("Nota mayor: " + nmy);
+            Console.WriteLine("Nota menor: " + nmn);
+            Console.WriteLine("Promedio:   " + prom);
+            Console.WriteLine("Condicion:  " + condicion);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Clase2/Ejercisio2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercisio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string familia, condicion, seguro;
+            int hijos, escolar;
+            double      
+           dinero = 0, dinescolar = 0, 
+           conf = 0, seg = 0, total = 0;
+
+            Console.WriteLine("Nombre de la Familia: ");
+            familia = Console.ReadLine();
+
+            Console.WriteLine("Escriba la Cant. de hijos: ");
+            hijos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriba Cant de hijos en edad escolar: ");
+            escolar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Casada(c) o Viuda(v)");
+            condicion = Console.ReadLine();
+
+            Console.WriteLine("Posee Seguro Si(y) No(n):  ");
+            seguro = Console.ReadLine();
+
+ 
+            if (hijos <= 2)
+            {
+                dinero = 120;
+            }
+            else if (hijos <= 5 && hijos >= 3)
+            {
+                dinero = 150;
+            }
+            else if (hijos >= 6)
+            {
+                dinero = 180;
+            }
+            else
+            {
+                dinero = 0;
+            }
+         
+            dinescolar = 20 * escolar;
+        
+            if (condicion == "c")
+            {
+                conf = 50;
+                condicion = "Casada";
+            }
+            else
+            {
+                conf = 90;
+                condicion = "Viuda";
+            }
+           
+            if (seguro.Equals("y"))
+            {
+                seg = 0;
+            }
+            else
+            {
+                seg = 100;
+            }
+ 
+            total = dinero + dinescolar + conf + seg;
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Mensualidad Dada: " + total);
+            Console.ReadLine();
+        }
+    }
+}
