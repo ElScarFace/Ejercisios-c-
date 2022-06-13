ae7ecf88a2a0a72bcd4850c95bc1aa09d1ca23c0
Renzo <64338288+Ilomilo18@users.noreply.github.com>
2022-06-13 18:06:19 -0500

    Add files via upload

---
+++ Clase1/Clase1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            double sueldo;
+            double v1, v2, v3 ,cv;
+           
+
+            Console.WriteLine("Ingrese su sueldo: ");
+            sueldo = Convert.ToDouble(Console.ReadLine());
+           
+
+            Console.WriteLine("Ingrese la 1º Venta: ");
+            v1 = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("Ingrese la 2º Venta: ");
+            v2 = Convert.ToDouble(Console.ReadLine());
+           
+
+            Console.WriteLine("Ingrese la 3º Venta: ");
+            v3 = Convert.ToDouble(Console.ReadLine());
+
+            cv = (v1 + v2 + v3) * 0.1; 
+
+            double total;
+            total = sueldo + cv; 
+
+         
+       
+            Console.WriteLine(" Venta 1:    " + v1);
+            Console.WriteLine(" Venta 2:    " + v2);
+            Console.WriteLine(" Venta 3:    " + v3);
+            Console.WriteLine(" Comision:   " + cv);
+            Console.WriteLine(" Pago Total: " + total);
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Clase1/Ejercisio2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercisio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dinero, total;
+            int b100, b50, b20, b10, m5, m2;
+
+            b100 = 0;b50 = 0;b20 = 0;
+            b10 = 0;m5 = 0;m2 = 0;
+
+            Console.WriteLine("Ingrese la cantidad de dinero: ");
+            dinero = Convert.ToInt32(Console.ReadLine());
+            total = dinero;
+            if (dinero >= 100)
+            {
+                b100 = (dinero / 100);
+                dinero = dinero - (b100 * 100);
+            }
+            if (dinero >= 50)
+            {
+                b50 = (dinero / 50);
+                dinero = dinero - (b50 * 50);
+            }
+            if (dinero >= 20)
+            {
+                b20 = (dinero / 20);
+                dinero = dinero - (b20 * 20);
+            }
+            if (dinero >= 10)
+            {
+                b10 = (dinero / 10);
+                dinero = dinero - (b10 * 10);
+            }
+            if (dinero >= 5)
+            {
+                m5 = (dinero / 5);
+                dinero = dinero - (m5 * 5);
+            }
+            if (dinero >= 2)
+            {
+                m2 = (dinero / 2);
+                dinero = dinero - (m2 * 2);
+            }
+
+          
+            Console.WriteLine("  Billetes de 100: " + b100);
+            Console.WriteLine("  Billetes de 50 : " + b50);
+            Console.WriteLine("  Billetes de 20 : " + b20);
+            Console.WriteLine("  Billetes de 10: " + b10);
+            Console.WriteLine("  Monedas de 5:    " + m5);
+            Console.WriteLine("  Monedas de 2:    " + m2);
+            Console.WriteLine("  Monedas de 1:    " + dinero);
+            Console.ReadLine();
+        }
+    }
+}
